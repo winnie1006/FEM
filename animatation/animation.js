b2e73b78d210daad1d1442ef2db977913b34e708
winnie1006 <zhuyu941006@gmail.com>
2022-07-13 22:25:16 +0800

    init animatation

---
+++ animatation/animation.js
@@ -0,0 +1,23 @@
+import { styler, value, listen, pointer, spring } from "popmotion";
+
+const ball = document.querySelector('.box');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
+  });
